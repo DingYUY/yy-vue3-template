b6f1b81cef8e2e83a4bb7d3bc8c4ac8ab4e32141
丁雨阳 <dzyyyt@163.com>
2024-04-06 19:35:10 +0800

    feat: :art: 集成vue-router,pinia,axios二次封装

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import '@router/permission'
+
+import 'normalize.css'
+
+import '@/styles/index.scss'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(store)
 
-app.mount('#app')
+router.isReady().then(() => {
+  app.mount('#app')
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { type ConfigEnv, type UserConfigExport, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default ({ mode }: ConfigEnv): UserConfigExport => {
@@ -13,7 +12,7 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
     plugins: [vue(), VueDevTools()],
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
+        '@': resolve(__dirname, 'src')
       }
     },
     css: {
